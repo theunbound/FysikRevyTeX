e19d04160fd4b04d2edd0e2a354aeaf03b6d0b15
Kristoffer Levin Hansen <the.bentusi@gmail.com>
2024-05-02 18:44:28 +0200

    fortsat med rowwise_reader, med mindre ambitiøs autofortoklning

--- scripts/roles_reader.py
+++ scripts/roles_reader.py
@@ -1,20 +1,39 @@
 from helpers import rows_from_csv_etc
 
 def rowwise_csv( fn ):
-    """Det her er formatet, som create.py laver, når det får argumentet
-roles_sheet. Men med den forskel, at overskrift-cellerne, både i
-første række og kolonne er valgfri, rækkerne med ordantal bliver
-ignoreret, og behøver ikke at være der. Kolonnen med aktnavne
-ignoreres, og behøver ikke at være der. Enten filnavne eller titler
-kan udelades, eller byttes rundt, og vi prøver at finde ud af det
-alligevel.
+    """Det her er formatet, som create.py laver, når det får
+argumentet roles-sheet. Men med den forskel, at overskrift-kolonnen er
+valgfri. Rækkerne med ordantal bliver ignoreret, og behøver ikke at
+være der. Kolonnen med aktnavne ignoreres, og behøver ikke at
+udfyldes. For hvert nummer kan filnavn eller titel udelades. Hvis
+begge er angivet prioriteres filnavnet.
 
     """
 
+    translations = {}
+
     rows = rows_from_csv_etc( fn )
+    row_gen = row for row in rows
     title_col = 1 if\
         len( [ True for row in rows if not row[0] == '' ] ) / len( rows )\
         > 0.5 else 0
+
+    def gather_mat( info_row ):
+        title = info_row[ title_col + 1 ]
+        fn = info_row[ title_col + 2 ]
+        abbr = info_row[ title_col + 3 : ]
+
+        actor_row = next( row_gen )
+        if any( [ title_col + 1 : title_col + 2 ] ):
+            return gather_mat( actor_row )
+        actors = actor_row[ title_col + 3 : ]
+
+        desc_row = next( row_gen )
+        if any( [ title_col + 1 : title_col + 2 ] ):
+            return gather_mat( desc_row )
+        descs = desc_row[ title_col + 3 : ]
+
+        
 
     mats = [[]]
     for row in rows:
